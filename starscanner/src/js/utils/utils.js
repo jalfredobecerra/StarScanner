2ad6092f6838d287509735e87df863a129662b3e
Julian <julian19becerra@gmail.com>
2026-05-27 21:41:57 -0500

    Changes

--- starscanner/src/js/utils/utils.js
+++ starscanner/src/js/utils/utils.js
@@ -1,26 +1,43 @@
 export function formatTime(unixTimestamp) {
-    const date = new Date(unixTimestamp * 1000);
-    return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const date = new Date(unixTimestamp * 1000);
+  return date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
 }
 
 export function degreesToCompass(degrees) {
-    const directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
-    const index = Math.round(degrees / 22.5) % 16;
-    return directions[index];
+  const directions = [
+    "N",
+    "NNE",
+    "NE",
+    "ENE",
+    "E",
+    "ESE",
+    "SE",
+    "SSE",
+    "S",
+    "SSW",
+    "SW",
+    "WSW",
+    "W",
+    "WNW",
+    "NW",
+    "NNW",
+  ];
+  const index = Math.round(degrees / 22.5) % 16;
+  return directions[index];
 }
 
 export function generateUUID() {
-    return crypto.randomUUID();
+  return crypto.randomUUID();
 }
 
 export function clamp(value, min, max) {
-    return Math.min(Math.max(value, min), max);
+  return Math.min(Math.max(value, min), max);
 }
 
 export function debounce(fn, delay) {
-    let timer;
-    return function(...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => fn(...args), delay);
-    };
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
 }
--- starscanner/src/main.js
+++ starscanner/src/main.js
@@ -1,6 +1,3 @@
-import { initNavigation } from "./js/modules/app";
-import { detectLocation } from './js/modules/location.js';
-import { reverseGeocode } from './js/modules/location.js';
-
+import { initNavigation } from "./js/modules/app.js";
 
 initNavigation();
